ad32480701137fed24532668cdd7e0cff00807f7
Shashank Nellutla <snellutla26@gmail.com>
2024-11-21 13:18:37 -0500

    Add files via upload

---
+++ info.py
@@ -0,0 +1,74 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpg"
+about_me = "I'm Shashank Nellutla. I am a first year Industrial Engineering student at Georgia Tech. I am also a real esate salesperson. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "linkedin.com/in/shashank-nellutla-aaa024267"
+my_email_address = "snellutla26@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Industrial Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2027',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "MATH 2551", "PHYS 2211", "ECON 2100"], 
+    "names":["Intro to CS", "Multivariable Calculus", "Intro to Physics 1", "Economic Analysis and Policy Problems"], 
+    "semester_taken":["1st", "1st", "1st", "1st"],
+    "skills":["Python was hard", "3D Calculus", "AP Physics with Vectors", "Macroeconomics"],
+    }
+experience_data = {
+    "Lifeguard at a Neighborhood Pool": (["- Supervised the pool and surroundings with one other",
+                                                                          "- Safeguarded children from drowning", "- Earned lifeguard of the month"],"Images/lifeguard.jpg"),
+    "Marketing Intern at a Sports Apparel Store":(["- Made advertisements for the store's social media page",
+                                                           "- Increased customer visits by creating unique apparel"],"Images/marketing.jpg"),
+    "Marketing Lead at Fund-a-Box":(["- Lead a marketingteam of highschoolers for a startup app"],"Images/fundabox.png")
+
+}
+
+projects_data = {
+    "Fund-a-Box": "Led the marketing team for a home gardening based startup app funded by Microsoft and Women of AT&T",
+}
+
+programming_data = {
+    "Python": 70,
+    "Java": 70,
+    "HTML": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "HTML": "🔍",
+}
+spoken_icons = {"Telugu": "",
+    "English": "",
+    "Spanish": ""
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Telugu": "Native",
+    "Spanish": "Fluent",
+}
+leadership_data = {
+    "Co-founder/Vice President at AAI Ted-Ed": (["- Co-founder for Ted-Ed club at Alliance Academy for Innovation which hosted public speaking events"],"Images/teded.jpg"),
+
+}
+activity_data={
+    "FBLA State Leadership Conference": ["- 4th - Sports and Entertainment Management Case Study (January 2023)", 
+            "- 6th - Parliamentary Procedure (March 2022)"]
+}
