0806ac1ffaa444461c283aceb9e420131730b6a7
Shashank Nellutla <snellutla26@gmail.com>
2024-11-21 17:08:06 -0500

    Create 3_Weather.py

---
+++ pages/3_Weather.py
@@ -0,0 +1,72 @@
+import openmeteo_requests
+
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 52.52,
+	"longitude": 13.41,
+	"current": ["temperature_2m", "is_day", "precipitation", "rain"],
+	"hourly": ["temperature_2m", "relative_humidity_2m", "precipitation_probability", "precipitation", "rain", "visibility", "is_day"],
+	"temperature_unit": "fahrenheit",
+	"wind_speed_unit": "mph",
+	"precipitation_unit": "inch",
+	"forecast_days": 1
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Current values. The order of variables needs to be the same as requested.
+current = response.Current()
+current_temperature_2m = current.Variables(0).Value()
+current_is_day = current.Variables(1).Value()
+current_precipitation = current.Variables(2).Value()
+current_rain = current.Variables(3).Value()
+
+print(f"Current time {current.Time()}")
+print(f"Current temperature_2m {current_temperature_2m}")
+print(f"Current is_day {current_is_day}")
+print(f"Current precipitation {current_precipitation}")
+print(f"Current rain {current_rain}")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+hourly_precipitation_probability = hourly.Variables(2).ValuesAsNumpy()
+hourly_precipitation = hourly.Variables(3).ValuesAsNumpy()
+hourly_rain = hourly.Variables(4).ValuesAsNumpy()
+hourly_visibility = hourly.Variables(5).ValuesAsNumpy()
+hourly_is_day = hourly.Variables(6).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+hourly_data["precipitation_probability"] = hourly_precipitation_probability
+hourly_data["precipitation"] = hourly_precipitation
+hourly_data["rain"] = hourly_rain
+hourly_data["visibility"] = hourly_visibility
+hourly_data["is_day"] = hourly_is_day
+
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+print(hourly_dataframe)
