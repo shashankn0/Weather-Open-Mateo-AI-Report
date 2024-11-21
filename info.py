b8a9e8aa9ca06581c5b21cdf6bec786d9e90c704
Shashank Nellutla <snellutla26@gmail.com>
2024-11-21 15:07:44 -0500

    Update info.py

--- info.py
+++ info.py
@@ -9,7 +9,7 @@ about_me = "I'm Shashank Nellutla. I am a first year Industrial Engineering stud
 #CHANGE BELOW (OPTIONAL)
 linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
 email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
-instagram_image_url = 
+instagram_image_url = "images/instagram.jpg"
 
 #CHANGE BELOW
 my_linkedin_url = "linkedin.com/in/shashank-nellutla-aaa024267"
