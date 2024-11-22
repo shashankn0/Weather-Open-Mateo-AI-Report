94cf0906e442f0659c12826afaffe671bb4a887d
Shashank Nellutla <snellutla26@gmail.com>
2024-11-22 16:10:31 -0500

    Update ai.py

--- pages/ai.py
+++ pages/ai.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 import os
 
-model = genai.GenerativeModel("gemini-1.5-flash") #this is the free model of google gemini
+model = genai.GenerativeModel("AIzaSyAaATNzmhk_gSmABukpNjux9EO9IQhwt5E") #this is the free model of google gemini
 response = model.generate_content("Write a poem about how learning web development is fun!") #enter your prompt here!
 print(response.text) #dont forget to print your response!
