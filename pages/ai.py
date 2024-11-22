59a8a74ed8965204d38e8b05eed536b7515871b7
Shashank Nellutla <snellutla26@gmail.com>
2024-11-22 16:12:29 -0500

    Update ai.py

--- pages/ai.py
+++ pages/ai.py
@@ -1,6 +1,12 @@
 import google.generativeai as genai
 import os
+#AIzaSyAaATNzmhk_gSmABukpNjux9EO9IQhwt5E -- AI KEY
 
-model = genai.GenerativeModel("AIzaSyAaATNzmhk_gSmABukpNjux9EO9IQhwt5E") #this is the free model of google gemini
+curl \
+  -H "Content-Type: application/json" \
+  -d "{\"contents\":[{\"parts\":[{\"text\":\"Explain how AI works\"}]}]}" \
+  -X POST "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyAaATNzmhk_gSmABukpNjux9EO9IQhwt5E"
+
+model = genai.GenerativeModel("gemini-1.5-flash") #this is the free model of google gemini
 response = model.generate_content("Write a poem about how learning web development is fun!") #enter your prompt here!
 print(response.text) #dont forget to print your response!
