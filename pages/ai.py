bd0f51380cd5726c0b55d4fa9d0a51505fd2c22d
Shashank Nellutla <snellutla26@gmail.com>
2024-11-23 10:54:03 -0500

    Update ai.py

--- pages/ai.py
+++ pages/ai.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 import os
 #AIzaSyAaATNzmhk_gSmABukpNjux9EO9IQhwt5E -- AI KEY
-genai.configure(api_key=os.environ[AIzaSyAaATNzmhk_gSmABukpNjux9EO9IQhwt5E])
+genai.configure(api_key=os.environ["AIzaSyAaATNzmhk_gSmABukpNjux9EO9IQhwt5E"])
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Write a story about a magic backpack.")
 print(response.text)
