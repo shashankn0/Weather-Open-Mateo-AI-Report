017946eff48ef1d642aceafd6db57946a7070552
Shashank Nellutla <snellutla26@gmail.com>
2024-11-26 10:51:31 -0500

    Delete pages/ai.py

--- pages/ai.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-import os
-#AIzaSyAaATNzmhk_gSmABukpNjux9EO9IQhwt5E -- AI KEY
-
-export API_KEY=<AIzaSyAaATNzmhk_gSmABukpNjux9EO9IQhwt5E>
-genai.configure(api_key=os.environ[AIzaSyAaATNzmhk_gSmABukpNjux9EO9IQhwt5E])
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Write a story about a magic backpack.")
-print(response.text)
