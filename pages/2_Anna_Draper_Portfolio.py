8d2a8b800aefadb98ed927cd09f70277c5e43d21
annadraper <annadraper06@gmail.com>
2024-11-26 15:53:57 -0500

    Update 2_Anna_Draper_Portfolio.py

--- pages/2_Anna_Draper_Portfolio.py
+++ pages/2_Anna_Draper_Portfolio.py
@@ -14,8 +14,8 @@ about_me_section()
 # Sidebar Links
 def links_section():
     st.sidebar.header("Links")
-    st.sidebar.text("Connect with me on LinkedIn")
-    linkedin_link = f'<a href="{info2.my_linkedin_url}"><img src = "{info2.linkedin_image_url}" alt = "LockDownBrowser-2-1-2-09-912935199inkedIn" width = "75" height = "75"></a>'
+    st.sidebar.text("Connect with me on Instagram")
+    linkedin_link = f'<a href="{info2.my_insta_url}"><img src = "{info2.insta_image_url}" alt = "LockDownBrowser-2-1-2-09-912935199inkedIn" width = "75" height = "75"></a>'
     st.sidebar.markdown(linkedin_link, unsafe_allow_html=True)
     st.sidebar.text("Checkout my work")
     github_link = f'<a href="{info2.my_github_url}"><img src="{info2.github_image_url}" alt = "Github" width="65" height+"65"></a>'
