a1d960bdc448aff25ad76d3e218e433590e71cbb
annadraper <annadraper06@gmail.com>
2024-11-26 15:04:34 -0500

    Create info2.py

---
+++ info2.py
@@ -0,0 +1,75 @@
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/profile.JPG"
+about_me = "I'm Anna Draper. I am a first year Industrial Engineering student at Georgia Tech. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+instagram_image_url = "images/instagram.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "linkedin.com"
+my_instagram_url = "https://www.instagram.com/_annadraperr/"
+my_email_address = "annadraper06@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Industrial Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2028',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "MATH 1552", "PSYCH 1101", "ECON 2100"], 
+    "names":["Intro to CS", "Integral Calculus", "General Psychology", "Economic Analysis and Policy Problems"], 
+    "semester_taken":["1st", "1st", "1st", "1st"],
+    "skills":["Coding can be fun", "Undoing Differential Calc", "Kinda Boring...", "Micro and Macro"],
+    }
+experience_data = {
+    "Worked at Roswell Gymnastics": (["- Taught kids how to do cartwheels",
+                                                                          "- Rescued many kids from the foam pit"],"images/lifeguard.jpg"),
+    "Tropical Smoothie worker":(["- Made a lot of smoothies",
+                                                           "- Drank a lot of smoothies"],"images/marketing.jpg"),
+    "Hydralive IV Therapy Front Desk Manager":(["- Checked in customers and took their vitals"
+                                               "- Very boring"],"images/Fundabox.jpg")
+
+}
+
+projects_data = {
+    "Crochet Master": ["Crocheted animals for all my friends before they left for college", image]
+}
+
+programming_data = {
+    "Python": 70,
+    "Java": 30,
+    "HTML": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "HTML": "🔍",
+}
+spoken_icons = {
+    "English": "",
+    "Spanish": ""
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Spanish": "I can probably say like 3 phrases",
+}
+leadership_data = {
+    "Gymnastics Team Captain": (["- Junior and Senior year captain of my gymnastics team"],"images/teded.jpg"),
+
+}
+activity_data={
+    "Gymnastics": ["- 13 years of competitive gymnastics", 
+            "- Won a lot of medals", ]
+}
