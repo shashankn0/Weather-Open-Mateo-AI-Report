27e43db7aa59a5740993fea43fb902734411ecbd
annadraper <annadraper06@gmail.com>
2024-11-26 12:49:52 -0500

    Update 4_Report.py

--- pages/4_Report.py
+++ pages/4_Report.py
@@ -73,8 +73,7 @@ if cityFound==True:
             st.subheader("What should you wear today??")
             st.write("Ask about what your should wear or whether the outfit you have planned will fit with today's conditions!")
             st.session_state.conversation_history = []
-            question = st.text_input(" ")
-            st.text_input.placeholder("Your question...")
+            question = st.text_input(" ", placeholder="Your question...")
             if st.button("Submit"):
                 if question:
                     model = genai.GenerativeModel("gemini-1.5-flash")
